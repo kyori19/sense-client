0225865a947165d5b32b71c74cd433fc06fb58de
kyori19 <kyori@accelf.net>
2022-12-01 16:37:20 +0900

    Add flashing dot

--- lib/Screen/Screen.cpp
+++ lib/Screen/Screen.cpp
@@ -3,18 +3,35 @@
 namespace Screen {
     Adafruit_SSD1306 display(128, 64, &Wire, 2); // NOLINT(cppcoreguidelines-interfaces-global-init)
 
+    String lines[8];
+
     void setup() {
         display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
-        display.clearDisplay();
+        display.fillScreen(WHITE);
         display.setTextSize(1);
         display.setTextColor(WHITE);
-        display.setCursor(0, 0);
 
-        display.print("test");
-        display.display();
+        for (auto & line : lines) {
+            line = "                     ";
+        }
+    }
+
+    void set_dot() {
+        lines[POS_DOT] = millis() % 1000 < 500 ? '.' : ' ';
+    }
+
+    void render_lines() {
+        display.setCursor(0, 0);
+        for (auto & line : lines) {
+            display.println(line);
+        }
     }
 
     void loop() {
+        set_dot();
 
+        display.clearDisplay();
+        render_lines();
+        display.display();
     }
 }
--- lib/Screen/Screen.h
+++ lib/Screen/Screen.h
@@ -3,6 +3,19 @@
 
 #include "Adafruit_SSD1306.h"
 
+// The display has 21x8 chars to display
+// _____________________
+// _____________________
+// _____________________
+// _____________________
+// _____________________
+// _____________________
+// _____________________
+// ____________________.
+
+#define POS_GENERATE(x, y) y][x
+#define POS_DOT POS_GENERATE(20, 7)
+
 namespace Screen {
     void setup();
     void loop();
